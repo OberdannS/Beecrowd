500e290f2067e2f986ae8b662f864ed15b23579a
Oberdan Santos <oberdanjsantos@gmail.com>
2024-11-15 20:49:33 -0300

    bee1001

---
+++ C#/bee1001.cs
@@ -0,0 +1,12 @@
+﻿namespace Beecrowd;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int x = a + b;
+        Console.WriteLine("X = {0}", x);
+    }
+}
