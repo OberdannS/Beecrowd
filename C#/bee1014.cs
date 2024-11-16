0342ac27350325e55f9a4ddbdf577e5df8def92f
Oberdan Santos <oberdanjsantos@gmail.com>
2024-11-15 22:02:15 -0300

    bee1014

---
+++ C#/bee1014.cs
@@ -0,0 +1,13 @@
+﻿namespace Beecrowd;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int km = int.Parse(Console.ReadLine());
+        double litros = double.Parse(Console.ReadLine());
+        double kml = km / litros;
+        Console.WriteLine(kml.ToString("F3") + " km/l");
+
+    }
+}
