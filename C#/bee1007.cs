cae46924a724f85f24e4479976a2a7f869968fee
Oberdan Santos <oberdanjsantos@gmail.com>
2024-11-15 21:16:33 -0300

    Create bee1007.cs

---
+++ C#/bee1007.cs
@@ -0,0 +1,15 @@
+﻿namespace Beecrowd;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+        int d = int.Parse(Console.ReadLine());
+        int diferenca = (a * b) - (c * d);
+        Console.WriteLine("DIFERENCA = {0}", diferenca);
+
+    }
+}
