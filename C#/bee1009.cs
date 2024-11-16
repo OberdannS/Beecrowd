dabe0bae941d066b068a2950170f377103c65950
Oberdan Santos <oberdanjsantos@gmail.com>
2024-11-15 21:29:59 -0300

    bee1009

---
+++ C#/bee1009.cs
@@ -0,0 +1,14 @@
+﻿namespace Beecrowd;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string nome = Console.ReadLine();
+        double salario = double.Parse(Console.ReadLine());
+        double vendas = double.Parse(Console.ReadLine());
+        double total = (vendas * 15 / 100) + salario;
+        Console.WriteLine("TOTAL = R$ " + total.ToString("F2"));
+        
+    }
+}
